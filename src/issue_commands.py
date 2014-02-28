0fee1ff314e01d73b9eca6143ecd7fe155310145
Rohan Bhargava <rohan.bhargava11@gmail.com>
2014-02-28 15:39:15 -0400

    working for parrot

--- src/issue_commands.py
+++ src/issue_commands.py
@@ -5,7 +5,7 @@ import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Point
 import actionlib
-
+from std_srvs.srv import Empty
 from tum_ardrone.msg import *
 def issue_commands(data):
     rospy.loginfo("waypoints: Got the target!!!")
@@ -19,7 +19,30 @@ def issue_commands(data):
     client.send_goal(goal)
     state=client.wait_for_result(rospy.Duration.from_sec(45.0))
     print state
-    rospy.set_param('check',state)
+    if state==True:
+	count=0
+	rospy.loginfo("issue_commands-: Reached the target, Toggle cam")
+	rospy.wait_for_service('ardrone/togglecam')
+        toggle_cam=rospy.ServiceProxy('ardrone/togglecam',Empty)
+	
+          #toggle_cam.wait_for_server()
+        try:
+
+	          state=toggle_cam()
+		  print state
+                  rospy.loginfo("color_tracker-: toogled the cam")
+        except rospy.ServiceException as exc:
+		rospy.loginfo("Service didn't process request"+str(exc))
+    else:
+	if count<3:
+
+		count=count+1
+		rospy.loginfo("issue_commands-: Didn't reach the target")
+		rospy.loginfo("issue_commands-: Trying"+str(count))
+		issue_commands(goal)
+        else:
+		rospy.loginfo("can't reach")
+    #rospy.set_param('check',state)
     
    
 
